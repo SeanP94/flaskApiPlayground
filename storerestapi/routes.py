6f3ca9c35113bfacbd63450be3b337df00172f4b
seanperry <seanep94@gmail.com>
2023-07-28 14:03:22 -0700

    Added delete and update functions

--- storerestapi/routes.py
+++ storerestapi/routes.py
@@ -15,10 +15,11 @@ def get_stores():
 def get_all_items():
     return {"items" : list(items.values())}
 
+
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
-        return stores[item_id], 201
+        return items[item_id], 201
     except KeyError:
         abort(404, message="Could not find item.")
     
@@ -68,3 +69,38 @@ def create_item():
     new_item = {**item_data, "id" : item_id}
     items[item_id] = new_item
     return new_item, 201
+
+
+# UPDATES
+
+@app.put('/item/<string:item_id>')
+def update_item(item_id):
+    item_data = request.get_json()
+    
+    # Make sure name and price are the only 2 keys.
+    if 'name' not in item_data or 'price' not in item_data and len(item_data) == 2:
+        abort(400, "Bad request, name and price are not in the item data.")
+    try:
+        item = items[item_id] # Get the item object
+        item |= item_data # Combine the name and price into the item_id.
+        return item, 201
+    except:
+        abort(404, message=f"Item id {item_id} not found.")
+
+# DELETES
+@app.delete('/item/<string:item_id>')
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message" : "Item deleted."}, 201
+    except KeyError:
+        abort(404, message=f"Item id {item_id} not found.")
+
+
+@app.delete('/item/<string:store_id>')
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message" : "Store deleted."}, 201
+    except KeyError:
+        abort(404, message=f"Item id {store_id} not found.")
